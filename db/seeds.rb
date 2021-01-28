c8572c0ade3871a9ed6a0378b57dd69b872dc648
jeremy beaussart <jeremy.beaussart@gmail.com>
2021-01-28 19:22:02 +0100

    add cloudinary key for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
-# puts 'destroy ingredients && Cocktails'
-# Ingredient.destroy_all
+puts 'destroy ingredients && Cocktails'
+Ingredient.destroy_all
 # Cocktail.destroy_all
 
-# puts 'Creating ingredient && Cocktail...'
-# lemon = { name: 'Lemon' }
-# ice = { name: 'Ice' }
-# mint = { name: 'Lint leaves' }
+puts 'Creating ingredient && Cocktail...'
+lemon = { name: 'Lemon' }
+ice = { name: 'Ice' }
+mint = { name: 'Lint leaves' }
 
 # pina_colada = { name: 'Pina colada' }
 # sex_on_the_beach = { name: 'Sex on the beach' }
 # malibu = { name: 'Malibu sunrise' }
 
-# [lemon, ice, mint].each do |attributes|
-#   ingredient = Ingredient.create!(attributes)
-#   puts "Created #{ingredient.name}"
-# end
+[lemon, ice, mint].each do |attributes|
+  ingredient = Ingredient.create!(attributes)
+  puts "Created #{ingredient.name}"
+end
 
 # [pina_colada, sex_on_the_beach, malibu].each do |attributes|
 #   cocktail = Cocktail.create!(attributes)
 #   puts "Created #{cocktail.name}"
 # end
 
-# puts 'Finished!'
+puts 'Finished!'
